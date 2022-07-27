dbef9a92ca74e2887aba2e4a27aff5f13ce3a4f7
Maryulianti <99313361+Maryulianti@users.noreply.github.com>
2022-07-27 22:55:34 +0800

    Add files via upload

---
+++ assets/javascriptsaya.js
@@ -0,0 +1,12 @@
+function keluar() {
+    var txt;
+    if (confirm("Apakah Anda ingin Keluar??!")) {
+      txt = "You pressed O";
+      <a href="index.php" class="logout_btn"></a>
+    } else {
+      txt = "You pressed Cancel!";
+      <a href="index.php" class="logout_btn"></a>
+    }
+    document.getElementById("demo").innerHTML =  <a href="index.php" class="logout_btn"></a>;
+  
+}
